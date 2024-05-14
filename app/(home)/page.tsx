0dfb969f752f67d255114e636099cfeb50bf394d
pgj0116 <83294916+pgj0116@users.noreply.github.com>
2024-05-15 01:46:11 +0900

    Loading Components and Parallel Request

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,23 +1,22 @@
+import Link from "next/link";
+
 export const metadata = {
   title: "Home",
 };
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 async function getMovies() {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-  return fetch(URL).then((response) => response.json());
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return fetch(API_URL).then((response) => response.json());
 }
-export default async function Tomato() {
-  // const [movies, setMovies] = useState();
-  // const getMovies = async () => {
-  //   const response = await fetch(
-
-  //   );
-  //   const json = await response.json();
-  //   setMovies(json);
-  // };
-  // useEffect(() => {
-  //   getMovies();
-  // }, []);
-  const response = await getMovies();
-  return <div>{JSON.stringify(response)}</div>;
+export default async function HomePage() {
+  const movies = await getMovies();
+  return (
+    <div>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movie/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </div>
+  );
 }
--- app/(movies)/movie/[id]/page.tsx
+++ app/(movies)/movie/[id]/page.tsx
@@ -1,3 +1,14 @@
-export default function Movie({ params: { id } }: { params: { id: string } }) {
-  return <h1>Movie {id}</h1>;
+import { API_URL } from "../../../(home)/page";
+async function getMopvie(id: string) {
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
+}
+
+export default async function MovieDetail({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const movie = await getMopvie(id);
+  return <h1>{movie.title}</h1>;
 }
